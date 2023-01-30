acac910d3aeafdd4ea5a669a1c5b3fc1c59a8406
Mena Mahany <mahanymena@gmail.com>
2023-01-30 22:50:59 +0200

    change log

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -9,5 +9,5 @@ export AWS_BUCKET=arn:aws:s3:::elasticbeanstalk-us-east-1-502317859992
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://udagram-api-dev22222222.us-east-1.elasticbeanstalk.com
 export NODE_OPTIONS=--openssl-legacy-provider
