ce5aede07b638b1682c8ac2d10a4da0081818835
Mena Mahany <mahanymena@gmail.com>
2023-01-31 17:06:10 +0200

    Review Update

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
 aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-502317859992/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-502317859992/
+eb setenv AWS_BUCKET=$AWS_BUCKET AWS_PROFILE=$AWS_PROFILE AWS_REGION=$AWS_DEFAULT_REGION JWT_SECRET=mysecretstring POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_PORT=5432 POSTGRES_USERNAME=$POSTGRES_USERNAME
